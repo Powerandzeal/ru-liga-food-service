a092c1a81c5ea8ae044c4aff79692b5af56b4bc8
Sergey <seregapanin23703@gmail.com>
2023-10-11 15:20:00 +0300

    удалил не тот пакет

---
+++ src/test/java/com/example/ruligafoodservice/RuLigaFoodServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ruligafoodservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuLigaFoodServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
