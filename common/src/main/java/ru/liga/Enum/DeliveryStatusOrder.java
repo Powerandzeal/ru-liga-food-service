caa467d513aef82b714d0d3e44b75996c71ab6b4
sereg <seregapanin23703@gmail.com>
2023-10-23 15:40:10 +0300

    починил my batis

---
+++ common/src/main/java/ru/liga/Enum/DeliveryStatusOrder.java
@@ -0,0 +1,11 @@
+package ru.liga.Enum;
+
+public enum DeliveryStatusOrder {
+    //DeliveryStatusOrder Status
+    DELIVERY_PENDING,
+    DELIVERY_PICKING,
+    DELIVERY_DELIVERING,
+    DELIVERY_DENIED,
+    DELIVERY_REFUNDED,
+    DELIVERY_COMPLETE,
+}
